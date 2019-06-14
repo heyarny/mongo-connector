753822281773f378bb3837df7bca94261b8163a9
Arnold <arny@eingrad.com>
2019-06-14 09:15:28 +0200

    + oops

--- run_connector.sh
+++ run_connector.sh
@@ -2,4 +2,4 @@
 
 echo "Starting mongo-connector: ${MAIN} -> ${TARGET} with Arguments: ${ARGS}"
 
-mongo-connector --doc-manager elastic2_doc_manager --main ${MAIN} --target-url ${TARGET}
+mongo-connector --doc-manager elastic2_doc_manager --main ${MAIN} --target-url ${TARGET} ${ARGS}
